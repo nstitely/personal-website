3f8698fcc8f64f1bf85b0225c95931f15773f3ac
nick <stitely28@gmail.com>
2026-06-14 00:56:19 -0400

    Implement automated deployment and update now page

--- garden/src/pages/now.js
+++ garden/src/pages/now.js
@@ -11,20 +11,28 @@ const NowPage = () => {
       
       <h1>Now</h1>
      
-      <p><em>Updated June 2, 2026</em></p>
+      <p><em>Updated June 14, 2026</em></p>
       
       <h2>What I'm Doing Now:</h2>
       
-      <h2>Working & Programming</h2>
-     
+      <h2>Working</h2>
+
       <p>Working as an IT Help Desk Technician.</p>
+
+      <h2>Programming</h2>
+      
       <p>Building this website using Gatsby and React.</p>
+      <p>Automating updates to this website.</p>
+      <p>Prisoner's Dilemma Simulation Project - Python</p>
+      <p>Trying to code daily.</p>
 
       <h2>Hobbies</h2>
      
       <h2>Books</h2>
-     
-      <p>Currently reading - The Song of Kali and The Way of Kings.</p>
+
+      <p>Finished reading - The Song of Kali.</p>
+      <p>Currently reading - The Way of Kings.</p>
+
       <p>Want to read:</p>
       <ul>
         <li>Ilyum</li>
@@ -33,24 +41,22 @@ const NowPage = () => {
         <li>Essential Keats</li>
       </ul>
      
-      <h2>Music</h2>
-     
-      <p>Waiting for Vince Staples's new Album - Cry Baby. This Friday, June 5th!!!!!</p>
-
       <h2>Video Games</h2>
      
       <p>Playing video games:</p>
       <ul>
         <li>Megabonk</li>
-        <li>Mini Motorways</li>
+        <li>Elden Ring</li>
+        <li>Hades</li>
       </ul>
 
       <p>Want to play:</p>
     <ul>
         <li>Deadlock</li>
-        <li>Elden Ring</li>
-        <li>Hades</li>
         <li>Everything is Crab</li>
+        <li>Destiny 2?</li>
+        <li>Rim World</li>
+
       </ul>
 
     </main>
